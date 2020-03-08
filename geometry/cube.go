4972cb9dd6047eb12ff86ad73b48f374c99c1ac6
rksmannem <rksmannem@gmail.com>
2020-03-07 22:15:25 -0600

    zero/negative edges are not allowed

--- geometry/cube.go
+++ geometry/cube.go
@@ -1,6 +1,11 @@
 package geometry
 
+import "fmt"
+
 //CubeVolume ...
-func CubeVolume(n int) int {
-	return n * n * n
+func CubeVolume(n int) (int, error) {
+	if n <= 0 {
+		return 0, fmt.Errorf("zero/negetive edge is not allowed: %v", n)
+	}
+	return n * n * n, nil
 }
